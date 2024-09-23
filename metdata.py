866abf355af8eb3dd6392ab99ebc77066e3e2634
Robert Oppedal <rob@torshov.biz>
2024-09-23 22:40:25 +0200

    starta så smått på no greier

---
+++ metdata.py
@@ -0,0 +1,17 @@
+import datetime, csv
+
+
+lokalStasjon = "datafiler/trykk_og_temperaturlogg_rune_time.csv"
+
+dato = list()
+tidspunkt = list()
+temperatur = list()
+
+
+
+with open(lokalStasjon, "r") as localWeather:
+    weather = csv.DictReader(localWeather, delimiter=";")
+    for line in weather:
+        dato.append(line["Dato og tid"])
+        temperatur.append(line["Temperatur (gr Celsius)"])
+print(f"dato: {dato}, temperatur: {temperatur}")
