7242d5d3c9787db8911c92d63b1af1b7b997fe88
DJDaisy <272257@uis.no>
2024-09-25 12:57:48 +0200

    Uppdatert og plotted
    Har en litt ødelaqt graf

--- Trykk_barometer.py
+++ Trykk_barometer.py
@@ -1,4 +1,6 @@
 import csv
+import datetime
+import matplotlib.pyplot as plt
 
 def datafil2_MET_lister():
     Trykk_barometer = list()
@@ -13,12 +15,52 @@ def datafil2_MET_lister():
 
         datafil2_MET.close()
     return Trykk_absolutt, Trykk_barometer
-Trykk_absloutt, Trykk_barometer = datafil2_MET_lister()
+Trykk_absolutt, Trykk_barometer = datafil2_MET_lister()
 
 Trykk_barometer_komma = [(point.replace(",",".")) for point in Trykk_barometer]
+Trykk_absolutt_komma = [(point.replace(",",".")) for point in Trykk_absolutt]
 cleaned_data = [float(item) for item in Trykk_barometer_komma if item]
-print(cleaned_data)
+
+
     
     
+
+lokalStasjon = "GitHub/dataAnalyse/datafiler/trykk_og_temperaturlogg_rune_time.csv"
+dato = []
+
+def datetimeConverter(date_time_string):
+    try:
+        format1 = "%m.%d.%Y %H:%M"
+        date_time = datetime.datetime.strptime(date_time_string, format1)
+        return date_time
+    except:
+        pass
+    try:
+        format2 = "%m/%d/%Y %I:%M:%S %p"
+        date_time = datetime.datetime.strptime(date_time_string, format2)
+    except:
+        try:
+            format3 = "%m/%d/%Y %H:%M:%S %p"
+            date_time = datetime.datetime.strptime(date_time_string, format3)
+        except:
+            pass
+    return date_time
+with open(lokalStasjon, "r") as localWeather:
+        weather = csv.DictReader(localWeather, delimiter=";")
+        for line in weather:
+            date = dato.append(datetimeConverter(line["Dato og tid"]))
+        
+
+                 
+plt.plot(dato, Trykk_absolutt_komma)
+plt.title("Pressure vs. Time")
+plt.xlabel("Date and Time")
+plt.ylabel("Pressure (Absolutt)")
+plt.xticks(rotation=45)  # Rotate x-axis labels for readability
+plt.tight_layout()  # Adjust layout to fit labels
+plt.show()
+
+
+print(date)
 
 #print(Trykk_barometer_komma)
