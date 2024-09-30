f819a047b78d8e9373e9e94c8c780831baa17083
DJDaisy <272257@uis.no>
2024-09-30 23:45:36 +0200

    Fremdrift

--- Datafiler_lister.py
+++ Datafiler_lister.py
@@ -6,19 +6,19 @@ def Datafil1_lister():
     Tid_norsknormaltid = list()
     Lufttemperatur = list()
     Lufttrykk = list()
-    with open("GitHub/dataAnalyse/datafiler/temperatur_trykk_met_samme_rune_time_datasett.csv"
+    with open("dataAnalyse/datafiler/temperatur_trykk_met_samme_rune_time_datasett.csv"
             , "r", encoding="utf-8") as datafil1:
         leser_filen = csv.reader(datafil1, delimiter= ";")
         next(leser_filen)
         for hver_rekke in leser_filen:
-            Tid_norsknormaltid.append(hver_rekke[2])
-            Lufttemperatur.append(hver_rekke[3])
-            Lufttrykk.append(hver_rekke[4])
+            Tid_norsknormaltid.append(hver_rekke[2].replace(",","."))
+            Lufttemperatur.append(hver_rekke[3].replace(",","."))
+            Lufttrykk.append(hver_rekke[4].replace(",","."))
         datafil1.close()
     return Tid_norsknormaltid, Lufttemperatur, Lufttrykk
 
 # For å hente listene i en annen kode
-Tid_norsknormaltid, Lufttemperatur, Lufttrykk = Datafil1_lister()
+
 
 
 
@@ -26,17 +26,19 @@ Tid_norsknormaltid, Lufttemperatur, Lufttrykk = Datafil1_lister()
 def datafil2_MET_lister():
     Trykk_barometer = list()
     Trykk_absolutt = list()
-    Datoer_Met= list()
-    with open("GitHub/dataAnalyse/datafiler/trykk_og_temperaturlogg_rune_time.csv",
+    Datoer_MET= list()
+    with open("dataAnalyse/datafiler/trykk_og_temperaturlogg_rune_time.csv",
         "r", encoding="UTF8") as datafil2_MET:
         lesing_fil = csv.reader(datafil2_MET, delimiter = ";")
         next(lesing_fil)
         for rekker in lesing_fil:
-                Datoer_MET.append(rekker[0])
-                Trykk_barometer.append(rekker[2])
-                Trykk_absolutt.append(rekker[3])
+                Datoer_MET.append(rekker[0].replace(",","."))
+                Trykk_barometer.append(rekker[2].replace(",","."))
+                Trykk_absolutt.append(rekker[3].replace(",","."))
+        
 
         datafil2_MET.close()
-    return Trykk_absolutt, Trykk_barometer
+    return Trykk_absolutt, Trykk_barometer, Datoer_MET
 # For å hente listene i en annen kode
-Trykk_absolutt, Trykk_barometer, Datoer_MET = datafil2_MET_lister()
+
+
--- Trykk_barometer.py
+++ Trykk_barometer.py
@@ -1,66 +1,44 @@
 import csv
 import datetime
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
-def datafil2_MET_lister():
-    Trykk_barometer = list()
-    Trykk_absolutt = list()
-    with open("GitHub/dataAnalyse/datafiler/trykk_og_temperaturlogg_rune_time.csv",
-        "r", encoding="UTF8") as datafil2_MET:
-        lesing_fil = csv.reader(datafil2_MET, delimiter = ";")
-        next(lesing_fil)
-        for rekker in lesing_fil:
-                Trykk_barometer.append(rekker[2])
-                Trykk_absolutt.append(rekker[3])
+from Datafiler_lister import Datafil1_lister, datafil2_MET_lister
 
-        datafil2_MET.close()
-    return Trykk_absolutt, Trykk_barometer
-Trykk_absolutt, Trykk_barometer = datafil2_MET_lister()
+# Load data
+Tid_norsknormaltid, Lufttemperatur, Lufttrykk = Datafil1_lister()
+Trykk_absolutt, Trykk_barometer, Datoer_MET = datafil2_MET_lister()
 
-Trykk_barometer_komma = [(point.replace(",",".")) for point in Trykk_barometer]
-Trykk_absolutt_komma = [(point.replace(",",".")) for point in Trykk_absolutt]
-cleaned_data = [float(item) for item in Trykk_barometer_komma if item]
-
-
-    
-    
-
-lokalStasjon = "GitHub/dataAnalyse/datafiler/trykk_og_temperaturlogg_rune_time.csv"
-dato = []
-
-def datetimeConverter(date_time_string):
-    try:
-        format1 = "%m.%d.%Y %H:%M"
-        date_time = datetime.datetime.strptime(date_time_string, format1)
-        return date_time
-    except:
-        pass
-    try:
-        format2 = "%m/%d/%Y %I:%M:%S %p"
-        date_time = datetime.datetime.strptime(date_time_string, format2)
-    except:
+# Function to convert date strings to datetime objects
+def konverter_dato(dato_tid):
+    formater = ["%m.%d.%Y %H:%M", "%m/%d/%Y %I:%M:%S %p", "%m/%d/%Y %H:%M:%S %p"]
+    for formateringsformat in formater:
         try:
-            format3 = "%m/%d/%Y %H:%M:%S %p"
-            date_time = datetime.datetime.strptime(date_time_string, format3)
-        except:
-            pass
-    return date_time
-with open(lokalStasjon, "r") as localWeather:
-        weather = csv.DictReader(localWeather, delimiter=";")
-        for line in weather:
-            date = dato.append(datetimeConverter(line["Dato og tid"]))
-        
+            return datetime.datetime.strptime(dato_tid, formateringsformat)
+        except ValueError:
+            continue
+    return None
 
-                 
-plt.plot(dato, Trykk_absolutt_komma)
-plt.title("Pressure vs. Time")
-plt.xlabel("Date and Time")
-plt.ylabel("Pressure (Absolutt)")
-plt.xticks(rotation=45)  # Rotate x-axis labels for readability
-plt.tight_layout()  # Adjust layout to fit labels
+# Convert dates
+konverterte_datoer = [konverter_dato(dato) for dato in Datoer_MET]
+
+
+
+Trykk_barometer_sample = Trykk_barometer[::6]
+konverterte_datoer_barometer = konverterte_datoer[::6]
+
+
+
+# Plotting the dat
+
+
+plt.plot(konverterte_datoer, Trykk_absolutt, color='blue', label='Absolutt Trykk')
+plt.plot(konverterte_datoer_barometer, Trykk_barometer_sample, color='orange', label='Barometer Trykk')
+
+
+
+plt.xticks(rotation=45)
+
+
+# Display the plot
 plt.show()
-
-
-print(date)
-
-#print(Trykk_barometer_komma)
