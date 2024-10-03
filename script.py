3701a0544d9725e1ca71cdeb2b61fd694c7b483f
Noland <12329617+nol-and@users.noreply.github.com>
2024-10-03 16:31:21 +0200

    betatest

--- script.py
+++ script.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
+from apiMET import hent_sol_tider, solData
 import datetime, csv
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
 from matplotlib.dates import DateFormatter
+
 
 # Filplassering for data som skal brukes i programmet
 lokal_stasjon = "datafiler/trykk_og_temperaturlogg_rune_time.csv"
@@ -22,7 +24,6 @@ met_trykk = []
 # Spesifiserer indekser med onsket tidspunkt for solnedgang mellom
 # 11. juni 2021 klokka 17:31 til 12. juni 2021 klokka 03:05
 
-lokal_solned_indekser = [1130, 4572]
 
 def datetimeConverter(date_time_string):
     # Konverterer datoformatet som blir brukt i den lokale fila og gjør om til datetime
@@ -110,13 +111,13 @@ def metOpener():
                 met_dato.append(datetimeConverterMET(line["Tid(norsk normaltid)"]))
                 tempFloat = line["Lufttemperatur"].replace(",",".")
                 met_temperatur.append(float(tempFloat))
-                tempTrykk = line["Lufttrykk i havnivå"].replace(",",".")
+                tempTrykk = line["Lufttrykk i havnivaa"].replace(",",".")
                 met_trykk.append(float(tempTrykk))
 
-def punktSolned():
+def punktSolned(variabel):
     # Henter inn punkt manuel fra solned_indekser
-    p1 = [lokal_dato[lokal_solned_indekser[0]], lokal_temperatur[lokal_solned_indekser[0]]]
-    p2 = [lokal_dato[lokal_solned_indekser[1]], lokal_temperatur[lokal_solned_indekser[1]]]
+    p1 = [lokal_dato[variabel[0]], lokal_temperatur[variabel[0]]]
+    p2 = [lokal_dato[variabel[1]], lokal_temperatur[variabel[1]]]
     x = [p1[0], p2[0]]
     y = [p1[1], p2[1]]
     return x, y
@@ -124,7 +125,7 @@ def punktSolned():
 
 def plotter(x1, y1, label, ylab, sub):
     # Tar inn liste med x og y-verdier, og plotter dem.
-    # plt.gca().xaxis.set_major_formatter(DateFormatter("%m-%d %H"))
+    plt.gca().xaxis.set_major_formatter(DateFormatter("%m-%d %H"))
     plt.subplot(2, 1, sub)
     plt.plot(x1,y1, label=label)
     #plt.gca().yaxis.set_major_locator(MaxNLocator(nbins=15, integer=True))
@@ -133,8 +134,51 @@ def plotter(x1, y1, label, ylab, sub):
     plt.xlabel("Dato")
     plt.ylabel(ylab)
     
+    
+def solOppOgNed():
+    solopp, solned = solData()
+    opp = 2
+    ned = 2
+    for i in lokal_dato:
+        
+        if str(i.strftime('%H:%M')) == solopp.strftime('%H:%M'):
+            break
+        else:
+            opp += 1
+        
+    for i in lokal_dato:
+        
+        if str(i.strftime('%H:%M')) == solned.strftime('%H:%M'):
+            break
+        else:
+            ned += 1
+    return opp, ned
+
+
+def temperaturFall():
+    opp, ned =  "17:31", "03:05"
+    oppCount = 2
+    nedCount = 2
+    for i in lokal_dato:
+        
+        if str(i.strftime('%H:%M')) == opp:
+            break
+        else:
+            oppCount += 1
+        
+    for i in lokal_dato:
+        
+        if str(i.strftime('%H:%M')) == ned:
+            break
+        else:
+            nedCount += 1
+    return oppCount, nedCount
+
+
 
 def main():
+    
+
     
     # Apner filer og laster dem inn i lister
     opener()
@@ -143,6 +187,10 @@ def main():
     # Regner ut gjennomsnitt
     averageDateTime = gjennomsnitt(lokal_dato, lokal_temperatur, 30)
     
+    solOppOgNed()
+    temperaturFall()
+
+
     # Setter opp Subplots
     plt.subplots(2, 1)
     
@@ -150,7 +198,9 @@ def main():
     plotter(lokal_dato, lokal_temperatur, "Lokal Temperatur", "Temp", 1)
     plotter(averageDateTime[0], averageDateTime[1], "Gjennomsnittstemperatur", "Temp", 1)
     plotter(met_dato, met_temperatur, "MET Temperatur", "Temp", 1)
-    plotter(*punktSolned(), "Temperaturfall maksimal til minimal", "Temp", 1)
+    plotter(*punktSolned(solOppOgNed()), "Temperaturfall maksimal til minimal", "Temp", 1)
+    plotter(*punktSolned(temperaturFall()), "Temperaturfall maksimal til minimal", "Temp", 1)
+    
     
     # Nedre Subplot
     plotter(lokal_dato, lokal_abs_trykk, "Absolutt trykk", "Trykk", 2)
